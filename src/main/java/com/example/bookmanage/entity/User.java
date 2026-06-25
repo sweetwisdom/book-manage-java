d3c8649db455c2783dd8c0958ece09a18d75bd3f
sweetwisdom <sweetwisdom@qq.com>
2026-06-25 16:49:38 +0800

    build: upgrade to MyBatis-Plus and add Lombok dependency
    - Replace mybatis-spring-boot-starter with mybatis-plus-boot-starter 3.5.15
    - Add Lombok 1.18.36
    - Refactor User entity with @Data annotation

--- src/main/java/com/example/bookmanage/entity/User.java
+++ src/main/java/com/example/bookmanage/entity/User.java
@@ -1,36 +1,17 @@
 package com.example.bookmanage.entity;
 
+import lombok.Data;
+
 /**
  * 用户实体 - 对应数据库表
  */
+
+@Data
 public class User {
 
     private Long id;
     private String name;
     private Integer age;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
